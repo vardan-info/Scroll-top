40d52195cf5205e05679b2fad2edf727ff2195ff
vardan <69822867+vardan-info@users.noreply.github.com>
2024-04-24 18:49:29 +0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+
+let courses = ["HTML", "CSS", "Javascript", "React"];
+
+// Accessing Array Elements
+console.log(courses[0]);
+console.log(courses[1]);
+console.log(courses[2]);
+console.log(courses[3]);
